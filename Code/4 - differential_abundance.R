5a1e57fa28faf09da588ddd17aeae0ec990377f6
Jason Selwyn <jdselwyn@gmail.com>
2024-04-23 13:45:15 -0400

    redo model

--- Code/4 - differential_abundance.R
+++ Code/4 - differential_abundance.R
@@ -1,6 +1,8 @@
 #### Libraries ####
+library(multcomp)
 library(tidyverse)
 library(multidplyr)
+library(broom)
 library(lmerTest)
 library(qvalue)
 library(emmeans)
@@ -24,6 +26,268 @@ tank_data <- read_csv('../intermediate_files/normalized_tank_asv_counts.csv',
   select(-time) %>%
   rename(time = time_fac)
 
+
+
+#### Just time 0 between antibiotics ####
+t.test_jds <- function(x, y = NULL, 
+                       alternative = c("two.sided", "less", "greater"),
+           mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95,
+           ...){
+    alternative <- match.arg(alternative)
+    
+    if(!missing(mu) && (length(mu) != 1 || is.na(mu)))
+      stop("'mu' must be a single number")
+    if(!missing(conf.level) &&
+       (length(conf.level) != 1 || !is.finite(conf.level) ||
+        conf.level < 0 || conf.level > 1))
+      stop("'conf.level' must be a single number between 0 and 1")
+    if( !is.null(y) ) {
+      dname <- paste(deparse(substitute(x)),"and",
+                     deparse(substitute(y)))
+      if(paired)
+        xok <- yok <- complete.cases(x,y)
+      else {
+        yok <- !is.na(y)
+        xok <- !is.na(x)
+      }
+      y <- y[yok]
+    }
+    else {
+      dname <- deparse(substitute(x))
+      if (paired) stop("'y' is missing for paired test")
+      xok <- !is.na(x)
+      yok <- NULL
+    }
+    x <- x[xok]
+    if (paired) {
+      x <- x-y
+      y <- NULL
+    }
+    nx <- length(x)
+    mx <- mean(x)
+    vx <- var(x)
+    if(is.null(y)) {
+      if(nx < 2) stop("not enough 'x' observations")
+      df <- nx-1
+      stderr <- sqrt(vx/nx)
+      if(stderr < 10 *.Machine$double.eps * abs(mx))
+        stop("data are essentially constant")
+      tstat <- (mx-mu)/stderr
+      method <- if(paired) "Paired t-test" else "One Sample t-test"
+      estimate <-
+        setNames(mx, if(paired)"mean of the differences" else "mean of x")
+    } else {
+      ny <- length(y)
+      if(nx < 1 || (!var.equal && nx < 2))
+        stop("not enough 'x' observations")
+      if(ny < 1 || (!var.equal && ny < 2))
+        stop("not enough 'y' observations")
+      if(var.equal && nx+ny < 3) stop("not enough observations")
+      my <- mean(y)
+      vy <- var(y)
+      method <- paste(if(!var.equal)"Welch", "Two Sample t-test")
+      estimate <- c(mx,my)
+      names(estimate) <- c("mean of x","mean of y")
+      if(var.equal) {
+        df <- nx+ny-2
+        v <- 0
+        if(nx > 1) v <- v + (nx-1)*vx
+        if(ny > 1) v <- v + (ny-1)*vy
+        v <- v/df
+        stderr <- sqrt(v*(1/nx+1/ny))
+      } else {
+        stderrx <- sqrt(vx/nx)
+        stderry <- sqrt(vy/ny)
+        stderr <- sqrt(stderrx^2 + stderry^2)
+        df <- stderr^4/(stderrx^4/(nx-1) + stderry^4/(ny-1))
+      }
+      if(stderr < 10 *.Machine$double.eps * max(abs(mx), abs(my)))
+        stop("data are essentially constant")
+      tstat <- (mx - my - mu)/stderr
+    }
+    if (alternative == "less") {
+      pval <- pt(tstat, df)
+      cint <- c(-Inf, tstat + qt(conf.level, df) )
+    }
+    else if (alternative == "greater") {
+      pval <- pt(tstat, df, lower.tail = FALSE)
+      cint <- c(tstat - qt(conf.level, df), Inf)
+    }
+    else {
+      pval <- 2 * pt(-abs(tstat), df)
+      alpha <- 1 - conf.level
+      cint <- qt(1 - alpha/2, df)
+      cint <- tstat + c(-cint, cint)
+    }
+    cint <- mu + cint * stderr
+    names(tstat) <- "t"
+    names(df) <- "df"
+    names(mu) <- if(paired || !is.null(y)) "difference in means" else "mean"
+    attr(cint,"conf.level") <- conf.level
+    rval <- list(statistic = tstat, parameter = df, p.value = pval,
+                 conf.int = cint, estimate = estimate, null.value = mu,
+                 alternative = alternative, stderr_pair = c(stderrx, stderry),
+                 method = method, data.name = dname)
+    class(rval) <- "htest"
+    return(rval)
+}
+
+predose_effects <- tank_data %>%
+  filter(time == 'before',
+         exposure == 'D') %>%
+  nest_by(across(domain:species), asv_id) %>%
+  mutate(spread = abs(min(data$log2_cpm_norm) - max(data$log2_cpm_norm))) %>%
+  filter(spread > 1) %>%
+  reframe(ttest = list(with(data, t.test_jds(log2_cpm_norm[anti == 'N'], log2_cpm_norm[anti == 'A']))),
+          
+          broom::tidy(ttest)) %>%
+  mutate(fdr = p.adjust(p.value, method = 'fdr'),
+         q.value = safe_qvalue(p.value))
+
+predose_effects %>%
+  filter(q.value < 0.05) %>%
+  select(domain:species, asv_id, estimate) %>%
+  distinct 
+
+#### Just After Dose comparison ####
+postDose_effects <- tank_data %>%
+  filter(time == 'after') %>%
+  nest_by(across(domain:species), asv_id) %>%
+  mutate(spread = abs(min(data$log2_cpm_norm) - max(data$log2_cpm_norm))) %>%
+  filter(spread > 1) %>%
+  reframe(model = list(aov(log2_cpm_norm ~ anti * exposure, data = data)),
+          tidy(model) %>%
+            select(term, p.value) %>%
+            filter(term != 'Residuals') %>%
+            pivot_wider(names_from = term, values_from = 'p.value',
+                        names_prefix = 'p.value_') %>%
+            rename_with(~str_replace_all(., ':', 'X'))) %>%
+  mutate(across(starts_with('p.value'), ~p.adjust(., method = 'fdr'), .names = 'fdr_{.col}'),
+         across(starts_with('p.value'), ~safe_qvalue(.), .names = 'q.value_{.col}')) %>%
+  rename_with(~str_replace_all(., '_p.value_', '_'))
+
+#### Combine ####
+full_join(predose_effects,
+          postDose_effects,
+          by = join_by(domain, phylum, class, order, family, genus, species, asv_id)) %>%
+  select(asv_id, starts_with(fdr_or_qvalue)) %>%
+  mutate(across(starts_with(fdr_or_qvalue), ~. < alpha)) %>%
+  rename(fdr_preDose = fdr) %>%
+  
+  upset(data = ., 
+        intersect = select(., where(is.logical)) %>% colnames)
+ggsave('../Results/asv_upset.png', 
+       height = 12, width = 10, bg = 'white')
+
+# preT.test <- tst$ttest[[1]]; anova_model <- tst$model[[1]]
+make_plot_data <- function(preT.test, anova_model){
+  emmeans(anova_model, ~exposure * anti) %>%
+    cld(Letters = LETTERS) %>%
+    broom::tidy() %>%
+    mutate(time = 'after',
+           .group = str_trim(.group)) %>%
+    bind_rows(tibble(anti = c('N', 'A'),
+                     estimate = preT.test$estimate,
+                     std.error = preT.test$stderr_pair,
+                     .group = if_else(preT.test$p.value < alpha, '*', ''),
+                     time = 'before',
+                     exposure = 'pre')) %>%
+    mutate(time = factor(time, levels = c('before', 'after')))
+}
+
+
+
+full_join(predose_effects,
+          postDose_effects,
+          by = join_by(domain, phylum, class, order, 
+                       family, genus, species, asv_id)) %>%
+  filter(asv_id %in% c('ASV25', 'ASV8')) %>%
+  rowwise(asv_id) %>%
+  reframe(make_plot_data(ttest, model)) %>%
+
+  ggplot(aes(x = time, y = estimate, 
+             ymin = estimate - std.error,
+             ymax = estimate + std.error,
+             shape = exposure,
+             colour = anti)) +
+  geom_errorbar(width = 0.1,
+                position = position_dodge(0.5)) +
+  geom_point(position = position_dodge(0.5), 
+             size = 4) +
+  geom_text(data = . %>% filter(time == 'after', anti == 'A'),
+            aes(label = .group, y = Inf),
+            position = position_dodge(0.5),
+            vjust = 1, show.legend = FALSE) +
+  geom_text(data = . %>% filter(time == 'after', anti == 'N'),
+            aes(label = .group, y = Inf),
+            position = position_dodge(0.5),
+            vjust = 2, show.legend = FALSE) +
+  geom_text(data = . %>% filter(time == 'before', anti == 'N'),
+            aes(label = .group, y = Inf),
+            vjust = 1, colour = 'black', size = 12) +
+  facet_wrap(~asv_id)
+
+
+
+full_join(predose_effects,
+          postDose_effects,
+          by = join_by(domain, phylum, class, order, 
+                       family, genus, species, asv_id)) %>%
+  filter((fdr_exposure < alpha & fdr_anti < alpha) | 
+           fdr_antiXexposure < alpha) %>%
+  mutate(name = case_when(!is.na(species) & !is.na(family) ~ 
+                            str_c(family, '\n', 
+                                  species, ' (', asv_id, ')'),
+                          !is.na(genus) & !is.na(family) ~ 
+                            str_c(family, '\n', 
+                                  genus, ' (', asv_id, ')'),
+                          !is.na(family) ~ 
+                            str_c(family, '\n', 
+                                  '(', asv_id, ')'),
+                          !is.na(order) ~ 
+                            str_c(order, '\n', 
+                                  '(', asv_id, ')'),
+                          !is.na(phylum) ~ 
+                            str_c(phylum, '\n', 
+                                  '(', asv_id, ')'),
+                          TRUE ~ asv_id)) %>%
+  filter(!is.na(estimate)) %>%
+  rowwise(name) %>%
+  reframe(make_plot_data(ttest, model)) %>%
+  
+  ggplot(aes(x = time, y = estimate, 
+             ymin = estimate - std.error,
+             ymax = estimate + std.error,
+             shape = exposure,
+             colour = anti)) +
+  geom_errorbar(width = 0.1,
+                position = position_dodge(0.5)) +
+  geom_point(position = position_dodge(0.5), 
+             size = 4) +
+  geom_text(data = . %>% filter(time == 'after', anti == 'A'),
+            aes(label = .group, y = Inf),
+            position = position_dodge(0.5),
+            vjust = 1, show.legend = FALSE) +
+  geom_text(data = . %>% filter(time == 'after', anti == 'N'),
+            aes(label = .group, y = Inf),
+            position = position_dodge(0.5),
+            vjust = 2, show.legend = FALSE) +
+  geom_text(data = . %>% filter(time == 'before', anti == 'N'),
+            aes(label = .group, y = Inf),
+            vjust = 1, colour = 'black', size = 12) +
+  facet_wrap(~name, scales = 'free_y')
+ggsave('../Results/asvs_changing_postExposure.png', height = 15, width = 15)
+
+
+full_join(predose_effects,
+          postDose_effects,
+          by = join_by(domain, phylum, class, order, 
+                       family, genus, species, asv_id)) %>%
+  select(-ttest, -estimate1, -estimate2, -statistic, -parameter:-alternative,
+         -model) %>%
+  rename_with(~str_c('preDose_', .), .cols = all_of(c('estimate', 'p.value', 'fdr', 'q.value'))) %>%
+  write_csv('../Results/individual_asv_results.csv')
+
 #### Model ASVs ####
 if(file.exists('../intermediate_files/asv_models.rds.xz') & !refit_models){
   asv_models <- read_rds('../intermediate_files/asv_models.rds.xz')
@@ -39,7 +303,7 @@ if(file.exists('../intermediate_files/asv_models.rds.xz') & !refit_models){
     # filter(asv_id %in% c('ASV25', 'ASV8')) %>%
     
     partition(cluster) %>%
-    mutate(model = list(lmer(log2_cpm_norm ~ time + anti + health + 
+    mutate(model = list(lmer(log2_cpm_norm ~ time * anti + health + 
                                (1 | tank) + (1 | geno / fragment),
                              data = data)),
            
